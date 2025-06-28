9b1fc52a44295451956b16bd9781ec871119a40b
chaitanyadeep <chaitanyadeepchellu5510@gmail.com>
2025-06-28 15:45:25 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -9,11 +9,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
+app.use('/api/auth', authRoutes); // Routes
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true, useUnifiedTopology: true
-}).then(() => {
-  console.log('MongoDB connected');
-  app.listen(5000, () => console.log('Server running on http://localhost:5000'));
-}).catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('MongoDB Connected');
+    app.listen(5000, () => console.log('Server running on http://localhost:5000'));
+  })
+  .catch(err => console.log(err));
